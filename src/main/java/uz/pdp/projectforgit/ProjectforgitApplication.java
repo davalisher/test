86bb3684e623b76425779e578ce4299af857b992
davalisher <davalisher@gmail.com>
2019-04-09 13:40:41 +0500

    ss

---
+++ src/main/java/uz/pdp/projectforgit/ProjectforgitApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.projectforgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectforgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectforgitApplication.class, args);
+    }
+
+}
---
+++ src/main/java/uz/pdp/projectforgit/controller/HomeController.java
@@ -0,0 +1,8 @@
+package uz.pdp.projectforgit.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+}
